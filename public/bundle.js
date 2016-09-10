60d6e7b5b0c1d2f7ec5b2282aedeb2c558ca0fc8
Wang <dihwang@ebay.com>
2016-09-10 22:05:46 +0800

    config 03： local server； auto refresh when changes

--- public/bundle.js
+++ public/bundle.js
@@ -44,23 +44,13 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	//main.js
-	var greeter = __webpack_require__(1);
-	document.getElementById('root').appendChild(greeter());
-
+	eval("//main.js\nvar greeter = __webpack_require__(1);\ndocument.getElementById('root').appendChild(greeter());\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9hcHAvbWFpbi5qcz82YTRiIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBO0FBQ0E7QUFDQSIsImZpbGUiOiIwLmpzIiwic291cmNlc0NvbnRlbnQiOlsiLy9tYWluLmpzXG52YXIgZ3JlZXRlciA9IHJlcXVpcmUoJy4vZ3JlZXRlci5qcycpO1xuZG9jdW1lbnQuZ2V0RWxlbWVudEJ5SWQoJ3Jvb3QnKS5hcHBlbmRDaGlsZChncmVldGVyKCkpO1xuXG5cblxuLyoqKioqKioqKioqKioqKioqXG4gKiogV0VCUEFDSyBGT09URVJcbiAqKiAuL2FwcC9tYWluLmpzXG4gKiogbW9kdWxlIGlkID0gMFxuICoqIG1vZHVsZSBjaHVua3MgPSAwXG4gKiovIl0sInNvdXJjZVJvb3QiOiIifQ==");
 
 /***/ },
 /* 1 */
 /***/ function(module, exports) {
 
-	
-	//greeter.js
-	module.exports = function(){
-		var greet = document.createElement('div');
-		greet.textContent = 'Hi there and greetings!';
-		return greet;
-	}
+	eval("\n//greeter.js\nmodule.exports = function(){\n\tvar greet = document.createElement('div');\n\tgreet.textContent = 'Hi there and greetings!';\n\treturn greet;\n}//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9hcHAvZ3JlZXRlci5qcz82M2FiIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EiLCJmaWxlIjoiMS5qcyIsInNvdXJjZXNDb250ZW50IjpbIlxuLy9ncmVldGVyLmpzXG5tb2R1bGUuZXhwb3J0cyA9IGZ1bmN0aW9uKCl7XG5cdHZhciBncmVldCA9IGRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoJ2RpdicpO1xuXHRncmVldC50ZXh0Q29udGVudCA9ICdIaSB0aGVyZSBhbmQgZ3JlZXRpbmdzISc7XG5cdHJldHVybiBncmVldDtcbn1cblxuXG4vKioqKioqKioqKioqKioqKipcbiAqKiBXRUJQQUNLIEZPT1RFUlxuICoqIC4vYXBwL2dyZWV0ZXIuanNcbiAqKiBtb2R1bGUgaWQgPSAxXG4gKiogbW9kdWxlIGNodW5rcyA9IDBcbiAqKi8iXSwic291cmNlUm9vdCI6IiJ9");
 
 /***/ }
 /******/ ]);
-//# sourceMappingURL=bundle.js.map
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,18 @@
 module.exports = {
     // devtool: 'source-map',
-    devtool: 'cheap-module-source-map',
-    // devtool: 'eval-source-map',
+    // devtool: 'cheap-module-source-map',
+    devtool: 'eval-source-map',
     // devtool: 'cheap-module-eval-source-map',
     entry: __dirname + "/app/main.js",   //the only main file
     output: {
         path: __dirname + "/public",     //where the files output
         filename: 'bundle.js'            //the file name of output
+    },
+    devServer: {//local server config
+        contentBase: "./public",    //local server static folder
+        color: true,    //terminal output has color
+        historyApiFallback: true,   //no jump page
+        inline: true            //real time refresh
     }
 };
 
